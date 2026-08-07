bd21f47b183cc56651b263a320baca08ddab9874
francinnyrebecadgurgel-ui <francinnyrebecadgurgel@gmail.com>
2026-08-07 17:42:52 -0300

    Add files via upload

---
+++ uc15 imc/desconto.js
@@ -0,0 +1,36 @@
+let nomeCliente = "Francinny";
+let valorCompra = 650;
+let clienteVip = false;
+
+let percentualDesconto;
+let valorDesconto;
+let valorFinal;
+
+
+if (clienteVip) {
+    percentualDesconto = 20;
+} else if (valorCompra >= 500) {
+    percentualDesconto = 15;
+} else if (valorCompra >= 200) {
+    percentualDesconto = 10;
+} else {
+    percentualDesconto = 0;
+}
+
+
+valorDesconto = valorCompra * (percentualDesconto / 100);
+valorFinal = valorCompra - valorDesconto;
+
+
+console.log("Nome: " + nomeCliente);
+console.log("Valor da compra: R$ " + valorCompra.toFixed(2));
+console.log("Desconto: " + percentualDesconto + "%");
+console.log("Valor do desconto: R$ " + valorDesconto.toFixed(2));
+console.log("Valor final: R$ " + valorFinal.toFixed(2));
+
+
+if (valorFinal > 1000) {
+    console.log("Parabéns! Você ganhou frete grátis.");
+} else {
+    console.log("Frete será cobrado normalmente.");
+}
